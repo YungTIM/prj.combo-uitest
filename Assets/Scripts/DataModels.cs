dfcd0155f8cb8f143ae2670ebb6dbf15aca07d50
Tim Huynh <timhuynhx@gmail.com>
2019-08-29 10:19:44 -0700

    intit commit

---
+++ Assets/Scripts/DataModels.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataModels
+{
+    public enum DisplayScreen
+    {
+        None = 0,
+        Menu = 1,
+        Dojo = 2,
+        Select = 3
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DataModels;
+
+/// <summary>
+/// Class to manage switching UI screens.
+/// </summary>
+public class UIManager : MonoBehaviour
+{
+    private DisplayScreen _currentDisplayedScreen;
+
+    public delegate void ChangeScreen(int oldScreen, int newScreen);
+    public static event ChangeScreen OnChangeScreen;
+
+    void Start()
+    {
+        //SwitchScreen(displ))
+    }
+
+    private void SwitchScreen(DisplayScreen screenToDisplay)
+    {
+        if (OnChangeScreen != null && _currentDisplayedScreen != screenToDisplay)
+        {
+            Debug.Log("Switching screens from " + _currentDisplayedScreen.ToString() + " to " + screenToDisplay);
+        }
+
+        OnChangeScreen((int) _currentDisplayedScreen, (int)screenToDisplay);
+
+        _currentDisplayedScreen = screenToDisplay;
+    }
+}
