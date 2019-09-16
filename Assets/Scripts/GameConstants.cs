fc4afc3387bfc49c0a57911fab1fca48f9f4ce60
Tim Huynh <timhuynhx@gmail.com>
2019-09-16 11:05:21 -0700

    snap

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public static class GameConstants
+{
+    #region Colors
+    public static Color COLOR_TEXT_P_ONE = Color.blue;
+    public static Color COLOR_TEXT_P_TWO = Color.red;
+    public static Color COLOR_TEXT_P_THR = Color.red;
+    public static Color COLOR_TEXT_P_FOU = Color.red;
+    #endregion
+
+    #region GAME VALUES
+    /// <summary>
+    /// 
+    /// </summary>
+    #endregion
+
+    //this needs to be refactored into a look table-lookup using json
+    #region LocStrings 
+    public static string loc_s_press_start = "PRESS START";
+
+    public static string loc_s_arena = "ARENA";
+    public static string loc_s_online = "ONLINE";
+    public static string loc_s_dojo = "DOJO";
+    public static string loc_s_lore = "LORE";
+    public static string loc_s_settings = "SETTINGS";
+
+    public static string loc_s_volleyball = "VOLLEYBALL";
+    public static string loc_s_board = "BOARD THE PLATFORMS";
+    public static string loc_s_target = "TARGET TEST";
+    public static string loc_s_practice = "PRACTICE";
+
+    public static string loc_s_choose = "CHOOSE YOUR FIGHTER";
+    public static string loc_s_profile = "PROFILE";
+    #endregion
+
+    #region AnimStrings 
+    public static string SHOW_ANIMATOR_PARAMETER = "show";
+    public static string HIDE_ANIMATOR_PARAMETER = "hide";
+    #endregion
+}
---
+++ Assets/Scripts/ScreenHandlers/ScreenAnimatorHandler.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DataModels;
+
+/// <summary>
+/// Extendable class for a screen which handles animations and transitions
+/// </summary>
+public class ScreenAnimatorHandler : MonoBehaviour
+{
+    [SerializeField]
+    protected DisplayScreen myScreen;
+
+    [SerializeField]
+    protected Animator[] animators;
+
+    [SerializeField]
+    protected Text headerText;
+
+    void OnEnable()
+    {
+        UIManager.OnChangeScreen += HandleTransition;
+    }
+
+    void OnDisable()
+    {
+        UIManager.OnChangeScreen -= HandleTransition;
+    }
+
+    /// <summary>
+    /// Handle the transition, should we show or hide this screen. Params are ints because we can't pass enums through a delegate
+    /// </summary>
+    /// <param name="oldScreen"></param>
+    /// <param name="newScreen"></param>
+    private void HandleTransition(int oldScreen, int newScreen)
+    {
+        if ((int)myScreen == oldScreen)
+        {
+            Hide();
+        }
+        else if ((int)myScreen == newScreen)
+        {
+            Show();
+        }
+    }
+
+    public void Hide()
+    {
+        TogglePanelAnimator(false, animators);
+    }
+
+    public void Show()
+    {
+        TogglePanelAnimator(true, animators);
+        OnShow();
+        SetHeaderText();
+    }
+
+    private void TogglePanelAnimator(bool toggle, Animator[] animators)
+    {
+        for (int i = 0; i < animators.Length; i++)
+        {
+            animators[i].gameObject.SetActive(true);
+            animators[i].SetTrigger(toggle ? GameConstants.SHOW_ANIMATOR_PARAMETER : GameConstants.HIDE_ANIMATOR_PARAMETER);
+        }
+    }
+
+    public virtual void OnShow()
+    {
+    }
+
+    public virtual void SetHeaderText()
+    {
+    }
+}
